7f45d783901f940890434f06bf99983a9aa14610
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-28 20:55:09 +0100

    feat(game): add Ball class for ball movement and behavior

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
+from ball import Ball
 from paddle import Paddle
-
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -22,7 +24,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
