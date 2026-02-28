74546258c8e463a14cd634f072a62a981d6e99aa
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-28 20:50:29 +0100

    feat(game): refactor paddle implementation and integrate paddle controls

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from hmac import new
 from turtle import Turtle, Screen
-
+from paddle import Paddle
 
 
 screen = Screen()
@@ -10,41 +9,20 @@ screen.title("Pong")
 screen.tracer(0)
 
 
-
-paddle = Turtle()
-paddle.goto(350, 0)
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-
-
-
-
-
-
-
-
 
 
 screen.exitonclick()
