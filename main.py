9bb3d57b5c70d797ca8538bbff6541e2f76379c2
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-28 22:51:13 +0100

    feat(game): add reset functionality for ball when it misses a paddle

--- main.py
+++ main.py
@@ -36,6 +36,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset()
+
+    #Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset()
 
 
 screen.exitonclick()
