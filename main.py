439521d6b3e1b2b428c71d7f7be92cbaf16e3114
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-28 20:38:38 +0100

    feat(game): implement paddle movement and game setup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from hmac import new
 from turtle import Turtle, Screen
 
 
@@ -6,6 +7,36 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+
+
+paddle = Turtle()
+paddle.goto(350, 0)
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 
 
