a76295c54a5ee129a100c6abeef20c68bf3059a8
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-28 23:20:00 +0100

    feat(scoreboard): implement Scoreboard class to track and display scores

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from ball import Ball
 from paddle import Paddle
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,7 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -24,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -39,10 +40,12 @@ while game_is_on:
     #Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset()
+        scoreboard.l_point()
 
     #Detect when left paddle misses
     if ball.xcor() < -380:
         ball.reset()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
