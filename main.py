73704c12d9b2bb1b7f1237b07d52af67870151fa
TsukasaMita <tsukasa.mita@gmail.com>
2026-03-01 10:55:21 +0100

    refactor: remove unused `Turtle` import and reformat code for readability

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from ball import Ball
 from paddle import Paddle
 import time
@@ -29,20 +29,25 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    #Detect collision with wall
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    # Detect collision with paddle
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
         ball.bounce_x()
 
-    #Detect when right paddle misses
+    # Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset()
         scoreboard.l_point()
 
-    #Detect when left paddle misses
+    # Detect when left paddle misses
     if ball.xcor() < -380:
         ball.reset()
         scoreboard.r_point()
